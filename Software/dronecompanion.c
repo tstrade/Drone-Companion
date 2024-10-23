b519f71d9061ba7e377ac5b33f37959f77c510d8
Thomas Strade <99762399+tstrade@users.noreply.github.com>
2024-10-23 14:16:07 -0400

    Create dronecompanion.c - function definitions of own library

---
+++ Software/dronecompanion.c
@@ -0,0 +1,5 @@
+/* 
+This C file will contain all function definitions necessary for implementing the drone,
+excluding preexisting functions within any given component's library.
+  Ex.) Adafruit_BMP280.h, pitches.h, Servo.h, etc.
+*/
