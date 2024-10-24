f8cafc0886187448eb0a4dce3ec9217f3c2bee4a
Thomas Strade <tstrade@buffalo.edu>
2024-10-24 13:52:24 -0400

    Began dronecompanion library

--- Software/dronecompanion.c
+++ Software/dronecompanion.c
@@ -3,3 +3,4 @@ This C file will contain all function definitions necessary for implementing the
 excluding preexisting functions within any given component's library.
   Ex.) Adafruit_BMP280.h, pitches.h, Servo.h, etc.
 */
+
--- Software/dronecompanion.h
+++ Software/dronecompanion.h
@@ -1,4 +1,66 @@
+#ifndef DRONECOMPANION_H
+#define DRONECOMPANION_H
+
 /*
 This header file will contain all declarations necessary for our own library, consisting of
 functions that are not provided by any given component's library.
 */
+
+// Include all libraries needed for hardware
+#include <stdio.h>
+#include <stdlib.h>
+#include <pitches.h>
+#include <Servo.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
+
+// BMP280 Macros
+const int BMP_SCK = 13;
+const int BMP_MISO  = 12;
+const int BMP_MOSI  = 11;
+const int BMP_CS = 10;
+const float STD_SEA_LEVEL_HPA = 1013.25;
+const float DRONE_UPPER_LIM = 274.32;
+const float DRONE_LOWER_LIM = 152.40;
+
+// Adafruit Objects
+Adafruit_BMP280 bmp(BMP_CS);
+
+
+// BMP280 ground-level altitude
+float ground_ref;
+
+// Buzzer Macros
+const int BUZZER_PIN = 8;
+const int BUZZER_TONES = 6;
+
+// Radar Macros
+const int trigPin1 = 3;
+const int echoPin1 = 2;
+const int trigPin2 = 5;
+const int echoPin2 = 4;
+const int OBSTACLE_IN_RANGE = 200;
+const int USER_OUT_OF_RANGE = 100;
+const int USER_TOO_CLOSE = 80;
+const int HC_1 = 0;
+const int HC_2 = 1;
+
+// Radar dist/dur initialization
+long duration[2];
+long distance[2];
+
+// Radar Objects
+Servo hc1, hc2;
+
+// Buzzer Notes
+const int alarm[] = { NOTE_B4, 
+                      NOTE_F4, 
+                      NOTE_B4, 
+                      NOTE_F4,
+                      NOTE_B4,
+                      NOTE_F4};
+
+
+
+#endif
