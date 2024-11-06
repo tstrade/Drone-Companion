ec7c4afa1c1acb490a2e5afb2807a4a184ef7c29
Alex <112579189+at973@users.noreply.github.com>
2024-11-06 12:11:29 -0500

    Radar Functionality_V1
    Starting this now so that i dont put it off. Will add Radar setup code in V2. Will do testing on what is an acceptable distance from user for V2 or V3.

---
+++ Software/DoubleRadarTest/Radar_Functionality.ino
@@ -0,0 +1,27 @@
+//radar functionality//
+void setup() {
+  //setup radars pinout and input reading
+
+}
+
+void loop() {
+  //radar 1 will be pointing at user(side of motors 3 and 4)
+  /*
+  while(radarvalue > [specified distance]){
+    // ESC3.speed(2000);
+    // ESC4.speed(2000);
+  }
+  //ESC3.speed(1500);
+  //ESC4.speed(1500);
+
+  while(radarvalue < [Specified distance]){
+    //ESC1.speed(2000);
+    //ESC2.speed(2000);
+  }
+  //ESC3.speed(1500);
+  //ESC4.speed(1500);
+  */
+  //radar 2 will be pointing awy from user, at objects(side of motors 1 and 2)
+
+
+}
