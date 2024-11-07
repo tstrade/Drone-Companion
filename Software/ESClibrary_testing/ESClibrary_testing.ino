751be48e35d336d672556b65837ff1b89c909852
Alex <112579189+at973@users.noreply.github.com>
2024-11-07 11:33:47 -0500

    Added both functional and manual calibration and arming
    Currently motors only work when you run only one of them. have not tested manual code yet but i expect it to run similarly. When a motor beeps repeatedly forever, idk what that means. However, when a motor beeps repeatedly for about a couple seconds and stops and does it again, it means that it has not met the required throttle range. if i run motors 1 and 2, and i have motor 1 calibrated first, and then 2, motor 2 will beep repeatedly and motor 1 will work. and vise versa.

--- Software/ESClibrary_testing/ESClibrary_testing.ino
+++ Software/ESClibrary_testing/ESClibrary_testing.ino
@@ -1,79 +1,112 @@
 #include <ESC.h>
 
 ESC ESC1 (9,1000,2000,500);
-// ESC ESC2 (6,1000,2000,500);  
-// ESC ESC3 (5,1000,2000,500);
-// ESC ESC4 (3,1000,2000,500);
+ESC ESC2 (6,1000,2000,500);  
+ESC ESC3 (5,1000,2000,500);
+ESC ESC4 (3,1000,2000,500);
 // //int potValue;
 // //int analog;
 
 void setup() {
-//   Serial.begin(9600);
+////////////////////////////////calib()////////////////////////////////////
+  // //calibrate
+  // ESC1.calib();
+  // ESC1.stop();
+  // delay(1000);
 
-//   Serial.println("calibrating:");
+  // ESC2.calib();
+  // ESC2.stop();
+  // delay(8000);
 
-  ESC1.setCalibrationDelay(2000);
-  ESC1.calib(); 
+  // ESC3.calib();
+  // ESC3.stop();
+  // delay(1000);
+
+  // ESC4.calib();
+  // ESC4.stop();
+  // delay(1000);
+
+  // //arm
+  // ESC1.arm();
+  // delay(1000);
+
+  // ESC2.arm();
+  // delay(1000);
+
+  // ESC3.arm();
+  // delay(5000);
+
+  // ESC4.arm();
+  // delay(5000);
+//////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////Manual////////////////////////////////////
+  //calibrate
+  ESC1.speed(2000);
   ESC1.stop();
-  Serial.println("ESC1 done");
-
-  ESC2.setCalibrationDelay(3000);
-  ESC2.calib();
-  ESC2.stop();
-  Serial.println("ESC2 done");
-
-  ESC3.setCalibrationDelay(3000);
-  ESC3.calib();
-  ESC3.stop();
-  Serial.println("ESC3 done");
-
-  ESC4.setCalibrationDelay(3000);
-  ESC4.calib();
-  ESC4.stop();
-  Serial.println("ESC4 done");
-
-  Serial.println("}all ESCs done");
-
-  Serial.println("Arming ESCs{");
-
-
-  ESC1.arm();
+  delay(1000);
+  ESC1.speed(1000);
   ESC1.stop();
   delay(1000);
-  Serial.println("ESC1 Armed.");
 
-  ESC2.arm();
+  ESC2.speed(2000);
   ESC2.stop();
   delay(1000);
-  Serial.println("ESC2 Armed.");
+  ESC2.speed(1000);
+  ESC2.stop();
+  delay(1000);
 
-  ESC3.arm();
+  ESC3.speed(2000);
   ESC3.stop();
   delay(1000);
-  Serial.println("ESC3 Armed.");
+  ESC3.speed(1000);
+  ESC3.stop();
+  delay(1000);
 
-  ESC4.arm();
+  ESC4.speed(2000);
   ESC4.stop();
   delay(1000);
-  Serial.println("ESC4 Armed.");
+  ESC4.speed(1000);
+  ESC4.stop();
+  delay(1000);
 
-  Serial.println("}All ESCs Armed");
+  //arm
+  ESC1.speed(500);
+  ESC1.stop();
+  delay(1000);
+
+  ESC2.speed(500);
+  ESC2.stop();
+  delay(1000);
+
+  ESC3.speed(500);
+  ESC3.stop();
+  delay(1000);
+
+  ESC4.speed(500);
+  ESC4.stop();
+  delay(1000);
+//////////////////////////////////////////////////////////////////////////
 }
 
 void loop() {
  
   ESC1.speed(1500);
+  delay(1000);
   ESC1.stop();
-  delay(5000);
+  delay(1000);
   ESC2.speed(1500);
+  delay(1000);
   ESC2.stop();
-  delay(5000);
+  delay(1000);
   ESC3.speed(1500);
+  delay(1000);
   ESC3.stop();
-  delay(5000);
+  delay(1000);
   ESC4.speed(1500);
+  delay(1000);
   ESC4.stop();
-  delay(5000);
+  delay(1000)
   
   // delay(1000);
   // for (potValue = 1000; potValue <= 2000; potValue++) {
