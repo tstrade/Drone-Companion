1d60a931fe1dc154cbf218612424e5965a94f98e
Thomas Strade <99762399+tstrade@users.noreply.github.com>
2024-11-07 15:14:43 -0500

    Update ESC_Calibration.ino parallel calibration

--- Software/ESC_Calibration/ESC_Calibration.ino
+++ Software/ESC_Calibration/ESC_Calibration.ino
@@ -7,28 +7,30 @@ Servo ESC4;
 int analog;
 int potValue;
 
-void armESC(Servo ESC) {
-    ESC.write(2000);        // start off setting max throttle
-    delay(6000);            // give the ESC time to see it and make note
-    ESC.write(1000);       // go to low-throttle
-    delay(5000);            // this might need to be adjusted up/down not sure
-    ESC.write(0);
+void armESC() {
+    ESC1.write(2000);        // start off setting max throttle
+    ESC2.write(2000);
+    ESC3.write(2000);
+    ESC4.write(2000);
+    delay(8000);            // give the ESC time to see it and make note
+    ESC1.write(1000);       // go to low-throttle
+    ESC2.write(1000);
+    ESC3.write(1000);
+    ESC4.write(1000);
+    delay(3000);            // this might need to be adjusted up/down not sure
+    ESC1.write (500);        // turn totally off
+    ESC2.write (500);
+    ESC3.write (500);
+    ESC4.write (500);
 }
 
 void setup() {
   // Attach the ESC on pin 9
   ESC1.attach(9);
-  armESC(ESC1);
   ESC2.attach(3);
-  armESC(ESC2);
   ESC3.attach(6);
-  armESC(ESC3);
   ESC4.attach(5);
-  armESC(ESC4);
-  ESC1.write(500);
-  ESC2.write(500);
-  ESC3.write(500);
-  ESC4.write(500);
+  armESC();
   delay(1000);
 }
 
