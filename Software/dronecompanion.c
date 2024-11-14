b774253b8e80a4bdf9c7576a310b69578a93c32b
Thomas Strade <tstrade@buffalo.edu>
2024-11-14 11:23:39 -0500

    refactored software folder - unnecessary libraries, unzipped IR library, renamed test folders

--- Software/dronecompanion.c
+++
@@ -1,6 +0,0 @@
-/* 
-This C file will contain all function definitions necessary for implementing the drone,
-excluding preexisting functions within any given component's library.
-  Ex.) Adafruit_BMP280.h, pitches.h, Servo.h, etc.
-*/
-
