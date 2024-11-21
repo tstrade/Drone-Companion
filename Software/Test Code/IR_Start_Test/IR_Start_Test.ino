1c81682eff4878553afb70acd9b4d6617f1cf776
Alex <112579189+at973@users.noreply.github.com>
2024-11-20 22:50:18 -0500

    IR Startup
    This should work. I believe this is how we should go about polling the code from the IR remote. The only problem faced here is how to extract the integer out of the code. I assume it is quite easy but I am unable to troubleshoot it at the moment.

---
+++ Software/Test Code/IR_Start_Test/IR_Start_Test.ino
@@ -0,0 +1,92 @@
+#include <Servo.h>
+#include <IRremote.h>
+
+//define motor pins
+#define BACKRIGHT 9
+#define FRONTLEFT 3
+#define BACKLEFT 6
+#define FRONTRIGHT 5
+
+// Define the servo motors (ESCs)
+Servo motorBackRight;
+Servo motorFrontLeft;
+Servo motorBackLeft;
+Servo motorFrontRight;
+
+//Initialize IR reciever in an IRrecv object
+IRrecv recieverIR(7);
+
+//the code read from IR remote
+int IRCode = 0; //2 is On, 3 is Off
+
+int minMotorSpeed = 1000;  // Minimum throttle
+int maxMotorSpeed = 2000;  // Maximum throttle
+
+void armESC() {
+    // Set high throttle
+    delay(2000);
+    motorBackRight.write(maxMotorSpeed);
+    motorFrontLeft.write(maxMotorSpeed);
+    motorBackLeft.write(maxMotorSpeed);
+    motorFrontRight.write(maxMotorSpeed);
+    delay(5000);
+    // Set low throttle
+    motorBackRight.write(minMotorSpeed);
+    motorFrontLeft.write(minMotorSpeed);
+    motorBackLeft.write(minMotorSpeed);
+    motorFrontRight.write(minMotorSpeed);
+    delay(3000);   
+    // Arm ESCs
+    motorBackRight.write(500);
+    motorFrontLeft.write(500);
+    motorBackLeft.write(500);
+    motorFrontRight.write(500);
+}
+
+void setup() {
+  // Attach ESCs to the appropriate pins
+  motorFrontLeft.attach(3);  // Pin 3 for front left motor
+  motorFrontRight.attach(5); // Pin 5 for front right motor
+  motorBackLeft.attach(6);   // Pin 6 for back left motor
+  motorBackRight.attach(9);  // Pin 9 for back right motor
+  //calibration and arming sequence is time sensitive
+  armESC();
+
+  //setup IR
+  recieverIR.enableIRIn();
+}
+
+int checkIRCode(int &IRCode){
+  if(receiverIR.decode()) {
+    Serial.println(receiverIR.decodedIRData.command);
+    IRCode = receiverIR.decodedIRData.command; //this will not return the integer. I do not have the USB to check how to recieve the integer from the given code.
+    //P.S. I know where the USB is, it is on my desk. i will have it tomorrow, November 21st.
+    receiverIR.resume();
+  }
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(IRCode == 2){
+    //reset IRCode
+    IRCode = 0;
+    //loop so it doesnt leave this if statement
+    while(true){
+      //start motors
+      motorBackRight.write(minMotorSpeed);
+      motorFrontLeft.write(minMotorSpeed);
+      motorBackLeft.write(minMotorSpeed);
+      motorFrontRight.write(minMotorSpeed);
+      if(IRCode == 2){
+        //reset IRCode
+        IRCode = 0;
+        //detach motors
+        ESC1.detach();
+        ESC2.detach();
+        ESC3.detach();
+        ESC4.detach();
+      }
+    }
+  }
+}
