0cc890947a67ef667617c02a52960616a1915ddc
Thomas Strade <tstrade@buffalo.edu>
2024-11-30 19:45:30 -0500

    adjusting filter and other calculations

--- Software/Test Code/Stabalize_Test/Stabalize_Test.ino
+++ Software/Test Code/Stabalize_Test/Stabalize_Test.ino
@@ -28,7 +28,7 @@ const float setpoint = 0.0;  // Desired pitch/roll angle (0 degrees)
 float previousErrorPitch = 0, integralPitch = 0;
 float previousErrorRoll = 0, integralRoll = 0;
 const float kp = 1.0, ki = 0.05, kd = 0.5;  // PID constants
-const float alpha = 0.98; // Complementary filter weight
+const float alpha = 0.95; // Complementary filter weight
 
 
 // Time tracking
@@ -46,6 +46,8 @@ float motorFrontRightSpeed = hoverMotorSpeed;
 float motorBackLeftSpeed = hoverMotorSpeed;
 float motorBackRightSpeed = hoverMotorSpeed;
 
+// Might be worth it to explore an offset equation
+// as a function of time, if data drift continues to be a problem
 void findOffset(float samples) {
   int i;
 
@@ -93,7 +95,7 @@ void setup() {
 
   mpu.calibrateGyro(1000);
   mpu.setThreshold(3);
-  findOffset(1000);
+  findOffset(1000.0);
   Serial.println("MPU6050 initialized and calibrated.");
 
   // Attach ESCs
@@ -116,30 +118,62 @@ void setup() {
 }
 
 void computeAngles(float elapsedTime) {
-  // Read gyroscope and accelerometer
+  // Read and adjust gyroscope / accelerometer
   Vector gyro = mpu.readNormalizeGyro();
+  gyro.XAxis -= gyroError.X;
+  Serial.print(gyro.XAxis); Serial.print(", ");
+  gyro.YAxis -= gyroError.Y;
+  Serial.print(gyro.YAxis); Serial.print(", ");
+  gyro.ZAxis -= gyroError.Z;
+  Serial.print(gyro.ZAxis); Serial.print(", ");
+
   Vector accel = mpu.readNormalizeAccel();
+  accel.XAxis -= accelError.X;
+  Serial.print(accel.XAxis); Serial.print(", ");
+  accel.YAxis -= accelError.Y;
+  Serial.print(accel.YAxis); Serial.print(", ");
+  accel.ZAxis -= accelError.Z;
+  Serial.print(accel.ZAxis); Serial.print(", ");
 
   // Calculate pitch and roll from gyroscope
-  float gyroPitch = pitch + gyro.YAxis * elapsedTime - gyroError.Y;
-  float gyroRoll = roll + gyro.XAxis * elapsedTime - gyroError.X; 
+  float gyroPitch = pitch + gyro.YAxis * elapsedTime;
+  Serial.print(gyroPitch); Serial.print(", ");
+  float gyroRoll = roll + gyro.XAxis * elapsedTime; 
+  Serial.print(gyroRoll); Serial.print(", ");
 
   // Calculate pitch and roll from acceleromter
-  float accPitch = (atan2(accel.XAxis - accelError.X, accel.ZAxis - accelError.Z) * 180)/PI;
-  float accRoll = (atan2(accel.YAxis - accelError.Y, accel.ZAxis - accelError.Z) * 180.0)/PI;
-  
-  // Apply complementary filter
-  pitch = (1 - alpha) * gyroPitch + alpha * accPitch;
-  roll = (1 - alpha) * gyroRoll + alpha * accRoll;
-  yaw += gyro.ZAxis * elapsedTime - gyroError.Z;
+  //float accPitch = (atan2(accel.XAxis - accelError.X, accel.ZAxis - accelError.Z) * 180.0)/PI;
+  //float accRoll = (atan2(accel.YAxis - accelError.Y, accel.ZAxis - accelError.Z) * 180.0)/PI;
 
+  float accRoll = (atan(accel.YAxis / sqrt(pow(accel.XAxis, 2) + pow(accel.ZAxis, 2))) * 180 / PI) ;
+  Serial.print(accRoll); Serial.print(", ");
+  float accPitch = (atan(-1.0 * accel.XAxis / sqrt(pow(accel.YAxis, 2) + pow(accel.ZAxis, 2))) * 180 / PI); 
+  Serial.print(accPitch); Serial.print(", ");
+
+  // Apply complementary filter
+  float newPitch = (1 - alpha) * gyroPitch + alpha * accPitch;
+  Serial.print(newPitch); Serial.print(", ");
+  pitch = (4.0 * pitch + newPitch) / 5.0;
+  float newRoll = (1 - alpha) * gyroRoll + alpha * accRoll;
+  Serial.print(newRoll); Serial.print(", ");
+  roll = (4.0 * roll + newRoll) / 5.0;
+  yaw += gyro.ZAxis * elapsedTime;
+
+  // Log values for debugging
+  //Serial.print("Pitch: "); 
+  Serial.print(pitch); Serial.print(", ");
+  //Serial.print("   Roll: "); 
+  Serial.print(roll); Serial.print(", ");
 }
 
 // PID control function
 float PIDControl(float currentAngle, float previousError, float integral, float elapsedTime) {
   float error = setpoint - currentAngle;
+  Serial.print(error); Serial.print(", ");
   integral += error * elapsedTime;
+  Serial.print(integral); Serial.print(", ");
   float derivative = (error - previousError) / elapsedTime;
+  Serial.print(derivative); Serial.print(", ");
   previousError = error;
 
   return (kp * error + ki * integral + kd * derivative);
@@ -147,45 +181,44 @@ float PIDControl(float currentAngle, float previousError, float integral, float 
 
 void adjustMotors(float pitchCorrection, float rollCorrection) {
   // Scale corrections to keep adjustments moderate
-  float pitchScale = 0.2; // Adjust scaling factor if needed
-  float rollScale = 0.3;
+  float pitchScale = 0.25; // Adjust scaling factor if needed
+  float rollScale = 0.25;
 
   // Calculate motor speed adjustments
-  float newMotorFrontLeftSpeed = hoverMotorSpeed - pitchCorrection * pitchScale - rollCorrection * rollScale;
-  float newMotorFrontRightSpeed = hoverMotorSpeed + pitchCorrection * pitchScale - rollCorrection * rollScale;
-  float newMotorBackLeftSpeed = hoverMotorSpeed - pitchCorrection * pitchScale + rollCorrection * rollScale;
-  float newMotorBackRightSpeed = hoverMotorSpeed + pitchCorrection * pitchScale + rollCorrection * rollScale;
-
-   // Log values for debugging
-  Serial.print("Pitch: "); Serial.print(pitch);
-  Serial.print("   Roll: "); Serial.print(roll);
+  float newMotorFrontLeftSpeed = hoverMotorSpeed - pitchCorrection * pitchScale + rollCorrection * rollScale;
+  float newMotorFrontRightSpeed = hoverMotorSpeed - pitchCorrection * pitchScale - rollCorrection * rollScale;
+  float newMotorBackLeftSpeed = hoverMotorSpeed + pitchCorrection * pitchScale + rollCorrection * rollScale;
+  float newMotorBackRightSpeed = hoverMotorSpeed + pitchCorrection * pitchScale - rollCorrection * rollScale;
 
   // Fine adjustment for motor speeds (smoothing and balancing)
-  //       i.e., motor speeds can only change by increments of 20% of the 
-  //             difference between the new and current speeds (can be tuned)
+  // Bias the old speed by 60% (can be changed based on observations)
   motorFrontLeftSpeed = (3 * motorFrontLeftSpeed + 2 * newMotorFrontLeftSpeed) / 5;  
   motorFrontRightSpeed = (3 * motorFrontRightSpeed + 2 * newMotorFrontRightSpeed) / 5;
   motorBackLeftSpeed = (3 * motorBackLeftSpeed + 2 * newMotorBackLeftSpeed) / 5;
   motorBackRightSpeed = (3 * motorBackRightSpeed + 2 * newMotorBackRightSpeed) / 5;
   
   // Apply limits for more gradual adjustments (1 microsecond is approx. 0.18 degrees)
-  float maxSpeedDifference = 30;  // Limit the max difference 60 microseconds
+  float maxSpeedDifference = 40;  // Limit the max difference 80 microseconds
   motorFrontLeftSpeed = constrain(motorFrontLeftSpeed, hoverMotorSpeed - maxSpeedDifference, hoverMotorSpeed + maxSpeedDifference);
   motorFrontRightSpeed = constrain(motorFrontRightSpeed, hoverMotorSpeed - maxSpeedDifference, hoverMotorSpeed + maxSpeedDifference);
   motorBackLeftSpeed = constrain(motorBackLeftSpeed, hoverMotorSpeed - maxSpeedDifference, hoverMotorSpeed + maxSpeedDifference);
   motorBackRightSpeed = constrain(motorBackRightSpeed, hoverMotorSpeed - maxSpeedDifference, hoverMotorSpeed + maxSpeedDifference);
 
-  // Write speeds to motors
-  motorFrontLeft.writeMicroseconds(motorFrontLeftSpeed);
+  // Write speeds to motors (left side tends to overpower - keep an eye on this)
+  motorFrontLeft.writeMicroseconds(motorFrontLeftSpeed - 100);
   motorFrontRight.writeMicroseconds(motorFrontRightSpeed);
-  motorBackLeft.writeMicroseconds(motorBackLeftSpeed);
+  motorBackLeft.writeMicroseconds(motorBackLeftSpeed - 100);
   motorBackRight.writeMicroseconds(motorBackRightSpeed);
 
   // Debugging: Print motor speeds
-  Serial.print("   FL: "); Serial.print(motorFrontLeftSpeed);
-  Serial.print("   FR: "); Serial.print(motorFrontRightSpeed);
-  Serial.print("   BL: "); Serial.print(motorBackLeftSpeed);
-  Serial.print("   BR: "); Serial.println(motorBackRightSpeed);
+  //Serial.print("   FL: "); 
+  Serial.print(motorFrontLeftSpeed); Serial.print(", ");
+  //Serial.print("   FR: "); 
+  Serial.print(motorFrontRightSpeed); Serial.print(", ");
+  //Serial.print("   BL: "); 
+  Serial.print(motorBackLeftSpeed); Serial.print(", ");
+  //Serial.print("   BR: "); 
+  Serial.println(motorBackRightSpeed);
 }
 
 void loop() {
