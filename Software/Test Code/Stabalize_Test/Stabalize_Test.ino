b43e5e0ddaf0315388b224ee787e87886884fb43
Thomas Strade <tstrade@buffalo.edu>
2024-12-02 00:39:05 -0500

    its so over - one of the motor mounts broke during testing

--- Software/Test Code/Stabalize_Test/Stabalize_Test.ino
+++ Software/Test Code/Stabalize_Test/Stabalize_Test.ino
@@ -2,17 +2,19 @@
 #include <Servo.h>
 #include <MPU6050.h>
 #include <IRremote.h>
+#include "PinChangeInterrupt.h"
 
 // IR sensor
 IRrecv receiverIR(7);
-int IRCode = 0;
+volatile int IRCode = 0;
 
 // Servo motors (ESCs)
 Servo FL, FR, BL, BR;
 int speeds[4];
 
 // Motor speed parameters
-const int minMotorSpeed = 1000, maxMotorSpeed = 2000, hoverMotorSpeed = 1200;
+const int minMotorSpeed = 1000, maxMotorSpeed = 2000;
+int hoverMotorSpeed = 1850;
 
 // MPU6050 instance
 MPU6050 mpu;
@@ -23,8 +25,8 @@ typedef struct {
 } Error;
 
 Error gyroError = {0, 0, 0}, accelError = {0, 0, 0};
-float previousErrorPitch = 0.0, integralPitch = 0.0, PitchPID;
-float previousErrorRoll = 0.0, integralRoll = 0.0, RollPID;
+float previousPitch = 0.0, integralPitch = 0.0, PitchPID;
+float previousRoll = 0.0, integralRoll = 0.0, RollPID;
 float pitch = 0.0, roll = 0.0, yaw = 0.0;
 
 // Time tracking
@@ -47,7 +49,6 @@ void setup() {
 
   // Setup IR sensor
   receiverIR.enableIRIn();
-  receiverIR.stopTimer();
 
   // Initialize and calibrate the MPU6050
   Serial.println(F("Initializing MPU6050"));
@@ -76,28 +77,23 @@ void setup() {
   BR.attach(6);
 
   applySpeeds(speeds);
-  /*
-  receiverIR.restartTimer();
+  
   Serial.println(F("Plug in the ESCs - wait for confirmation beeps\n"));
   while (IRCode != 2) { checkIRCode(); }
-  receiverIR.stopTimer();
-  */
 
   delay(1000);
   previousTime = micros();
 }
 
 void loop() {
-  receiverIR.restartTimer();
   checkIRCode();
-  receiverIR.stopTimer();
-
   unsigned long currentTime = micros();
-  // Sampling rate = 555 Hz
-  if (currentTime - previousTime >= 1800 && IRCode != 3) {
+  if (IRCode == 8) hoverMotorSpeed = 1700;
+  // Sampling rate = 2000 Hz
+  if (currentTime - previousTime >= 500 && IRCode != 3) {
     float elapsedTime = (currentTime - previousTime) / 1e6; // Convert to seconds
     previousTime = currentTime;
-
+    
     // Compute angles and apply PID
     computeAngles(elapsedTime);
     PIDControl(elapsedTime);
@@ -117,16 +113,7 @@ void applySpeeds(int speeds[]) {
   FL.writeMicroseconds(speeds[0]);
   FR.writeMicroseconds(speeds[1]);
   BL.writeMicroseconds(speeds[2]);
-  BR.writeMicroseconds(speeds[3]);  
-}
-
-void checkIRCode(){
-  if (receiverIR.decode()) { 
-    IRCode = receiverIR.decodedIRData.command; 
-    delay(10);
-    receiverIR.resume();
-  }
-  return;
+  BR.writeMicroseconds(speeds[3]);
 }
 
 void calibrateSensors(int samples) {
@@ -170,13 +157,9 @@ void computeAngles(float elapsedTime) {
   float accRoll = (atan2(accel.YAxis, sqrt(pow(accel.XAxis, 2) + pow(accel.ZAxis, 2))) * 180 / PI) ;
   float accPitch = (atan2(-1.0 * accel.XAxis, sqrt(pow(accel.YAxis, 2) + pow(accel.ZAxis, 2))) * 180 / PI); 
 
-  
-  int complement; 
-  // Apply complementary filter of alpha = 98%
-  //pitch = fmod(0.98 * gyroPitch + (0.02) * accPitch + 180.0, 360.0) - 180.0;
-  pitch = 0.98 * gyroPitch + (0.02) * accPitch;
-  //roll = fmod(0.98 * gyroRoll + (0.02) * accRoll + 180.0, 360.0) - 180.0;
-  roll = 0.98 * gyroRoll + (0.02) * accRoll;
+  // Apply complementary filter of alpha = 95%
+  pitch = (pitch + 0.98 * gyroPitch + (0.02) * accPitch) / 2.0;
+  roll = (roll + 0.98 * gyroRoll + (0.02) * accRoll) / 2.0;
 
   yaw += gyro.ZAxis * elapsedTime;
 }
@@ -186,48 +169,52 @@ void PIDControl(float elapsedTime) {
   // P = proportional gain -> magnitude of response to error
   // I = integral control -> how fast error is removed (increase ki = decrease response time)
   // D = derivate control -> how far in the future to predict rate of change
-  float kp = 1.181, ki = 0.109, kd = 0.0008; // PID constants for pitch
-  // error = SP - PV(t) (setpoint AKA 0.0 degreees - process variable at time t AKA current pitch)
-  float error = -pitch;
-  // u(t) = kp * e(t) - ki * integral_0^t e(x)dx + kd * d/dx * e(t)
-  // deviating from base formula due to drift accumuation
-  PitchPID = kp * error + ki * (integralPitch += error * elapsedTime) + kd * (error - previousErrorPitch) / elapsedTime;
-  previousErrorPitch = error;
+  float kp = 1.01, ki = 0.6, kd = 0.00003; // PID constants
 
-  kp = 1.042, ki = 0.103, kd = 0.0005; // PID constants for roll
-  error = -roll;
-  RollPID = kp * error + ki * (integralRoll + error * elapsedTime) + kd * (error - previousErrorPitch) / elapsedTime;
-  previousErrorRoll = error;
+  // u(t) = kp * e(t) + ki * integral_0^t e(x)dx + kd * d/dx * e(t)
+  PitchPID = kp * pitch + ki * (integralPitch += pitch * elapsedTime) + kd * (pitch - previousPitch) / elapsedTime;
+  previousPitch = pitch;
+  RollPID = kp * roll + ki * (integralRoll += pitch * elapsedTime) + kd * (pitch - previousPitch) / elapsedTime;
+  previousRoll = roll;
 }
 
 // Alternative to the abruptness of constrain()
 int softClamp(int value, int min, int max) {
-  if (value < min) return min + (value - min) / 6;
-  if (value > max) return max + (value - max) / 6;
+  if (value < min) return min + (value - min) / 5;
+  if (value > max) return max + (value - max) / 5;
   return value;
 }
 
 void adjustMotors() {
   // Scale corrections to keep adjustments moderate
-  float pitchScale = 0.3, rollScale = 0.3;
+  float scale = 0.2;
 
-  // Calculate motor speed adjustments w/ soft clamping to enforce bounds (1170 to 1230)
+  // positive pitch -> back side needs to increase speed
+  // positive roll -> right side needs to increase speed
   int targetSpeeds[4] = {
-    hoverMotorSpeed - PitchPID * pitchScale - RollPID * rollScale,
-    hoverMotorSpeed - PitchPID * pitchScale + RollPID * rollScale,
-    hoverMotorSpeed + PitchPID * pitchScale - RollPID * rollScale,
-    hoverMotorSpeed + PitchPID * pitchScale + RollPID * rollScale
+    hoverMotorSpeed + (-PitchPID - RollPID) * scale - 20, // Front left
+    hoverMotorSpeed + (-PitchPID + RollPID) * scale - 5, // Front right
+    hoverMotorSpeed + (PitchPID - RollPID) * scale + 20, // Back left
+    hoverMotorSpeed + (PitchPID + RollPID) * scale + 10 // Back right
   };
 
-  // Smooth transition 
+  // Calculate motor speed adjustments w/ soft clamping to smoothly enforce bounds
   for (int i = 0; i < 4; i++) {
-    speeds[i] = softClamp(0.8 * speeds[i] + 0.2 * targetSpeeds[i], 1170, 1230);
+    speeds[i] = softClamp(0.8 * speeds[i] + 0.2 * targetSpeeds[i], hoverMotorSpeed - 30, hoverMotorSpeed + 30);
   }
 
   // Write speeds to motors (left side tends to overpower - keep an eye on this)
   applySpeeds(speeds);
 
   debugOutput();
+}
+
+void checkIRCode() {
+  if (receiverIR.decode()) { 
+    IRCode = receiverIR.decodedIRData.command; 
+    delay(10);
+    receiverIR.resume();
+  }
 }
 
 void debugOutput() {
